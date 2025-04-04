1d7c48a3d4a08490de227358a3b9554891a6b617
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-04-03 22:22:56 -0600

    Fix tour bugs

--- js/Tour.js
+++ js/Tour.js
@@ -12,22 +12,39 @@ export default class Tour {
         anchor: 'pager-component .tabs > :last-child',
         content: 'The history page lists your completed goals and redeemed rewards.',
     }, {
-        anchor: 'goals-page-component check-button-component button',
-        content: `When you've completed this goal, click here to check it off and earn points.`,
-    }, {
-        anchor: 'goals-page-component menu-component',
-        content: 'Click here to edit or delete this goal.',
-    }, {
         anchor: '#action-button',
         content: 'Click here to create a new goal.',
     }, {
+        anchor: 'goals-page-component check-button-component button',
+        content: `After you've completed this goal, click here to check it off and earn points.`,
+        onError: _ => this.#createGoal(),
+    }, {
+        anchor: 'goals-page-component menu-component',
+        content: 'Click here to edit or delete this goal.',
+        onError: _ => this.#createGoal(),
+    }, {
         anchor: 'header-component menu-component',
-        content: 'Click here for additional information and options.',
+        content: 'The main menu contains additional information and options.',
     }];
+
+    static #createGoal() {
+        document.addEventListener('GoalCreated', this.#onGoalCreated, {once: true});
+        document.querySelector('#action-button').click();
+    }
+
+    static #onGoalCreated = event => {
+        setTimeout(async _ => {
+            const $goal = document.getElementById(event.detail.id);
+            await Promise.all($goal.getAnimations().map(a => a.finished));
+            this.#showHint(this.#resumeFromIndex);
+        });
+    };
+
+    static #resumeFromIndex;
 
     static prompt() {
         Modal.render({
-            content: `You're off to a great start! How about a quick tour to keep the momentum going?`,
+            content: `You're off to a great start! How about a quick tour?`,
             buttons: [
                 {label: 'No'},
                 {focus: true, label: 'Yes', onClick: _ => this.start()},
@@ -36,7 +53,8 @@ export default class Tour {
     }
 
     static start() {
-        this.#showHint(0);
+        location.hash = 'goals';
+        setTimeout(_ => this.#showHint(0));
     }
 
     static #showHint(index) {
@@ -45,7 +63,14 @@ export default class Tour {
             return;
         }
 
-        const {anchor, content} = this.#hints[index];
+        const {anchor, content, onError} = this.#hints[index];
+        const $anchor = document.querySelector(anchor);
+
+        if (!$anchor) {
+            onError();
+            this.#resumeFromIndex = index;
+            return;
+        }
 
         const backButton = index > 0
             ? {
@@ -63,7 +88,7 @@ export default class Tour {
         };
 
         Hint.render({
-            anchor,
+            anchor: $anchor,
             content,
             backButton,
             nextButton,
@@ -77,9 +102,7 @@ export default class Tour {
     }
 
     static #finish() {
-        Modal.render({
-            content: `That's the end of the tour. If you missed anything, you can restart it from
-                the main menu in the top-right corner.`,
-        });
+        Modal.render({content: `That's the end of the tour. If you missed anything, you can restart
+            it from the main menu at any time.`});
     }
 }
